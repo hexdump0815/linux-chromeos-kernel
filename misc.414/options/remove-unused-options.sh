a4c82cd5893c4397f8acf8e68127816f2ede5052
hexdump <hexdump0815@googlemail.com>
2021-07-22 19:48:55 +0200

    414: add some options to remove to make it boot

--- misc.414/options/remove-unused-options.sh
+++ misc.414/options/remove-unused-options.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /compile/source/linux-chromeos-4.19
+cd /compile/source/linux-chromeos-4.14
 
-for i in `cat /compile/doc/chromeos/misc.419/options/options-to-remove-generic.txt | grep -v ^#`; do
+for i in `cat /compile/doc/chromeos/misc.414/options/options-to-remove-generic.txt | grep -v ^#`; do
   echo $i
   ./scripts/config -d $i
 done
 
-for i in `cat /compile/doc/chromeos/misc.419/options/options-to-remove-special.txt | grep -v ^#`; do
+for i in `cat /compile/doc/chromeos/misc.414/options/options-to-remove-special.txt | grep -v ^#`; do
   echo $i
   ./scripts/config -d $i
 done
